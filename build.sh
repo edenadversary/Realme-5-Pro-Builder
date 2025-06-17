8a6ec41abef9f339cb604dd1b131b2ddda625839
edenadversary <143865198+edenadversary@users.noreply.github.com>
2025-06-17 10:22:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ ZIPNAME="TLWAT-RUI2-$(date '+%Y%m%d-%H%M').zip"
 
 [ $USE_PERSONAL_DEFCONFIG = "true" ] && DEFCONFIG="godspeed_suki_defconfig"
 
-[ $PATCH_KSUN_SUSFS = "true" ] && patch -p1 < ${HOME}/ksun_157.patch
-
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
 	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
@@ -51,13 +49,6 @@ CLANG_TRIPLE=aarch64-linux-gnu-
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
 mkdir out
-if
-patch -p1 < ksun_157.patch
-  return=1;
-else
-patch -p1 < ${HOME}/ksun_157.patch
-fi
-parch
 make $(echo $BUILD_FLAGS) $DEFCONFIG
 cp out/.config arch/arm64/configs/$DEFCONFIG
 rm -rf out
